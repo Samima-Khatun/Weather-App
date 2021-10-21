1f208c8e5a62e5ffcf8afcf9d8ad11e962933b37
unknown <samima7.sk@gmail.com>
2021-10-21 20:52:22 +0530

    version 2 updated

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
+require("dotenv").config();
 const express = require("express");
 const http = require("http");
 const bodyParser = require("body-parser");
-require("dotenv").config();
+const _ = require("lodash");
 
 const app = express();
 
+app.set("view engine", "ejs");
+
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(express.static("public"));
+
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + "/index.html");
+  res.render("index");
 });
 
 app.post("/", function (req, res) {
-  const query = req.body.cityName;
+  const query = _.upperFirst(req.body.cityName);
   const apikey = process.env.API_KEY;
   const unit = "metric";
   const url =
@@ -25,19 +30,30 @@ app.post("/", function (req, res) {
 
   http.get(url, function (response) {
     console.log(response.statusCode);
+    if (response.statusCode != 200) {
+      res.render("failure");
+    } else {
+      response.on("data", function (data) {
+        const weatherData = JSON.parse(data);
+        const temp = weatherData.main.temp;
+        const weatherDescription = _.upperFirst(
+          weatherData.weather[0].description
+        );
+        const icon = weatherData.weather[0].icon;
 
-    response.on("data", function (data) {
-      const weatherData = JSON.parse(data);
-      const temp = weatherData.main.temp;
-      const weatherDescription = weatherData.weather[0].description;
-      const icon = weatherData.weather[0].icon;
+        console.log(weatherDescription);
+        console.log(temp);
+        console.log(icon);
 
-      console.log(weatherDescription);
-      console.log(temp);
-      console.log(icon);
+        const imageUrl = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
 
-      const imageUrl = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
-
+        res.render("weather", {
+          city: query,
+          temp: temp,
+          weatherDescription: weatherDescription,
+          imageUrl: imageUrl,
+        });
+        /*
       res.write(
         "<h1>The temperature in " +
           query +
@@ -49,7 +65,12 @@ app.post("/", function (req, res) {
       res.write("<img src=" + imageUrl + ">");
 
       res.send();
-    });
+      */
+      });
+    }
+  });
+  req.on("error", function (e) {
+    res.send("Something went worng !!!");
   });
 });
 
